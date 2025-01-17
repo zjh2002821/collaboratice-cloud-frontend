557f1eda5a670b073542643fb091dde6b4ccec26
zjh <1243267647@qq.com>
2025-01-18 00:18:03 +0800

    协同云前端代码

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <BasicLayout />
+  </div>
+</template>
+
+<script setup lang="ts">
+import BasicLayout from '@/layouts/BasicLayout.vue'
+// import { healthUsingGet } from '@/api/mainController.ts'
+// import {useLoginUserStore} from "@/stores/useLoginUserStore.ts";
+
+// 已经改为在权限校验文件中获取
+// const loginUserStore = useLoginUserStore()
+// loginUserStore.fetchLoginUser()
+
+// healthUsingGet().then((res) => {
+//   console.log(res)
+// })
+</script>
+
+<style scoped></style>
